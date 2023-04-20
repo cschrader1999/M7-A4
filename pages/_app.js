85e8d7f675d97d4d64819e765cd75b800f944019
cschrader1999 <clschra1@asu.edu>
2023-04-20 10:05:14 -0700

    Module 7: Next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+import Link from 'next/link';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <nav>
+        <Link href="/">
+          <span>Home</span>
+        </Link>{' '}
+        <Link href="/about">
+          <span>About</span>
+        </Link>{' '}
+        <Link href="/SignUp">
+          <span>Sign Up</span>
+        </Link>{' '}
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
+
+export default MyApp;
